ad29f49a9fdcc951aed55752c5160b631a67276a
Corey Watts <cwatts@brightidea.com>
2014-10-27 18:49:02 -0700

    Fixing typos in option names

---
+++ console/migrations/m141028_013921_fix_typos_in_options.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141028_013921_fix_typos_in_options extends Migration
+{
+    public function up()
+    {
+        $this->update("option", array("name"=>"procrastination causing crises in money, work, or relationships"), array("name"=>"procrastinatino causing crises in money, work, or relationships"));
+        $this->update("option", array("name"=>"irrationality"), array("name"=>"rrationality"));
+
+    }
+
+    public function down()
+    {
+        echo "Why even revert this.";
+    }
+}
