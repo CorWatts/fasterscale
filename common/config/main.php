43c70d24945b479d166d0d7988d6001821311341
Corey Watts <corwatts@gmail.com>
2014-11-10 22:02:56 -0800

    Enabling schema caching, installed memcache.

--- common/config/main.php
+++ common/config/main.php
@@ -4,7 +4,14 @@ return [
     'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\FileCache',
+            'class' => 'yii\caching\MemCache',
+	    'servers' => [
+		[
+		    'host' => 'localhost',
+		    'port' => 11211,
+		    'weight' => 100
+		]
+	    ]
         ],
     ],
 ];
