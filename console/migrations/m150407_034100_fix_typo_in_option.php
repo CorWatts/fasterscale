445b7eef23f8a4bae003fc0fcc71fea6ae2b728f
Corey Watts <cwatts@brightidea.com>
2015-04-06 21:05:19 -0700

    Fixing typo in option table

---
+++ console/migrations/m150407_034100_fix_typo_in_option.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150407_034100_fix_typo_in_option extends Migration
+{
+    public function up()
+    {
+        $this->update("option", array("name"=>"making goals and lists you can't complete"), array("name"=>"makin goals and lists you can't complete"));
+    }
+
+    public function down()
+    {
+		echo "Why even revert this.";
+    }
+}
