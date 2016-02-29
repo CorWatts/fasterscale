4eec4c68d8e4fc10e81a49a9fdeed0722949e299
Corey Watts <cwatts@brightidea.com>
2016-02-28 22:01:45 -0800

    Improving alignment of <h3>s on front page

--- site/views/site/index.php
+++ site/views/site/index.php
@@ -15,24 +15,28 @@ $this->registerJsFile('/js/site/index.js', ['depends' => [\site\assets\AppAsset:
 		<div class="row">
 
 			<div class="col-md-4">
-				<div class="thumbnail">
+				<div class="well">
+					<div class="caption">
 				  <h3>Useful Analytics & Alerts</h3>
 					<canvas id='example-scores-line-chart'></canvas>
-					<div class="caption">
 						<p>Everyone loves analytics. View beautiful charts and graphs to gain additional insights to your emotional tendencies. Your daily checkin answers are stored so you can go back to any point in history and view what you were experiencing at that time. You can also choose to send a report to your friends if you score above a certain threshold, which opens up avenues for conversation.</p>
 					</div>
 				</div>
 			</div>
 
 			<div class="col-md-4">
-				<h2>The Faster Scale</h2>
+				<div class="thumbnail" style="border: none;">
+					<div class="caption">
+				<h3>The Faster Scale</h3>
 				<p>Use Michael Dye's popular and proven relapse prevention tool: The Faster Scale. Log the emotions you are experiencing in order to see how vulnerable to temptation you are in the moment. Check in daily to see the progress of your emotions over time.</p>
+					</div>
+				</div>
 			</div>
 
 			<div class="col-md-4">
-				<div class="thumbnail">
-				  <h3>Danger Score</h3>
+				<div class="well">
 				  <div class="caption">
+				    <h3>Danger Score</h3>
     		    <div class='alert alert-warning'>Definite warning signs. You aren't doing well. Take some time out, write out what you're feeling, and discuss it with someone.</div>
 				  	<p>A Danger Score is calculated for each checkin, allowing you to get a simple summary of how close to relapse you are. As your score goes up, you become more and more likely to relapse. Don't let temptation blindside you into relapse. See it coming before it arrives, and take preventative action.</p>
 				  </div>
