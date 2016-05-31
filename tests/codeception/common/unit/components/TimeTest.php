bc89e64ff3759e06e5f66e0140d04e0fb9d3c7d4
Corey Watts <cwatts@brightidea.com>
2016-05-30 22:50:09 -0700

    Finishing test coverage for \common\components\Time class

--- tests/codeception/common/unit/components/TimeTest.php
+++ tests/codeception/common/unit/components/TimeTest.php
@@ -17,7 +17,6 @@ date_default_timezone_set('UTC');
 
 class TimeTest extends TestCase
 {
-
     use Specify;
 
     public function setUp()
@@ -88,6 +87,27 @@ class TimeTest extends TestCase
       $this->specify('getLocalDate should function correctly', function () {
         expect("getLocalDate should correctly get the user's local date", $this->assertEquals(Time::getLocalDate(), (new DateTime("now", new DateTimeZone("America/Los_Angeles")))->format("Y-m-d")));
         expect("getLocalDate should correctly get the local date of a specified timezone", $this->assertEquals(Time::getLocalDate("UTC"), (new DateTime("now", new DateTimeZone("UTC")))->format("Y-m-d")));
+      });
+    }
+
+    public function testAlterLocalDate()
+    {
+      $this->specify('alterLocalDate should function correctly', function() {
+        expect('alterLocalDate should add one day to the local time', $this->assertEquals(Time::alterLocalDate('2016-05-01 00:00:00', '+1 day'), (new DateTime("2016-05-01 00:00:00 +1 day"))->format("Y-m-d")));
+        expect('alterLocalDate should subtract one week to the local time and change the year correctly', $this->assertEquals(Time::alterLocalDate('2016-01-01 04:03:00', '-1 week'), (new DateTime("2016-01-01 04:03:00 -1 week"))->format("Y-m-d")));
+      });
+    }
+
+    public function testGetUTCBookends()
+    {
+      $this->specify('getUTCBookends should function correctly', function() {
+        expect('getUTCBookends should return false if there is a space in the time string', $this->assertFalse(Time::getUTCBookends('2016-05-30 00:00:00')));
+        expect('getUTCBookends should return false if there is a space at the start of the time string', $this->assertFalse(Time::getUTCBookends(' 2016-05-30 00:00:00')));
+        expect('getUTCBookends should return false if there is a space at the end of the time string', $this->assertFalse(Time::getUTCBookends('2016-05-30 00:00:00 ')));
+        expect('getUTCBookends should return UTC bookend times from the Los_Angeles tz', $this->assertEquals(Time::getUTCBookends('2016-05-30'), ['2016-05-30 07:00:00', '2016-05-31 06:59:59']));
+        // with UTC
+        Yii::$app->user->identity->timezone = "UTC";
+        expect('getUTCBookends should return UTC bookend times from UTC tz', $this->assertEquals(Time::getUTCBookends('2016-05-30'), ['2016-05-30 00:00:00', '2016-05-30 23:59:59']));
       });
     }
 }
