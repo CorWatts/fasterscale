e1881c2fcb8dbe784201f276f1cbc69c93b5e833
Corey Watts <cwatts@brightidea.com>
2016-06-20 21:47:52 -0700

    Adding tests for calculateScore

---
+++ tests/codeception/common/unit/models/UserOptionTest.php
@@ -0,0 +1,291 @@
+<?php
+
+namespace tests\codeception\common\unit\models;
+
+use Yii;
+use tests\codeception\common\unit\TestCase;
+use Codeception\Specify;
+use common\models\UserOption;
+
+date_default_timezone_set('UTC');
+
+/**
+ * Time test
+ */
+
+class UserOptionTest extends TestCase
+{
+    use Specify;
+
+		public $singleSimpleBehaviors = [
+			[
+				'id' => 396,
+				'user_id' => 2,
+				'option_id' => 107,
+				'date' => '2016-06-17 04:12:43',
+				'option' => 
+				[
+					'id' => 107,
+					'name' => 'numb',
+					'category_id' => 6,
+				],
+			],
+		]; 
+
+		public $singleComplexBehaviors = [
+			[
+				'id' => 397,
+				'user_id' => 2,
+				'option_id' => 2,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 2,
+					'name' => 'resolving problems',
+					'category_id' => 1,
+				],
+			], [
+				'id' => 398,
+				'user_id' => 2,
+				'option_id' => 7,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 7,
+					'name' => 'making eye contact',
+					'category_id' => 1,
+				],
+			], [
+				'id' => 399,
+				'user_id' => 2,
+				'option_id' => 13,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 13,
+					'name' => 'less time/energy for God, meetings, and church',
+					'category_id' => 2,
+				],
+			], [
+				'id' => 400,
+				'user_id' => 2,
+				'option_id' => 19,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 19,
+					'name' => 'flirting',
+					'category_id' => 2,
+				],
+			], [
+				'id' => 401,
+				'user_id' => 2,
+				'option_id' => 31,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 31,
+					'name' => 'being resentful',
+					'category_id' => 3,
+				],
+			], [
+				'id' => 402,
+				'user_id' => 2,
+				'option_id' => 41,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 41,
+					'name' => 'co-dependent rescuing',
+					'category_id' => 3,
+				],
+			], [
+				'id' => 403,
+				'user_id' => 2,
+				'option_id' => 64,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 64,
+					'name' => 'too much caffeine',
+					'category_id' => 4,
+				],
+			], [
+				'id' => 404,
+				'user_id' => 2,
+				'option_id' => 95,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 95,
+					'name' => 'panicked',
+					'category_id' => 6,
+				],
+			], [
+				'id' => 405,
+				'user_id' => 2,
+				'option_id' => 97,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 97,
+					'name' => 'hopeless',
+					'category_id' => 6,
+				],
+			], [
+				'id' => 406,
+				'user_id' => 2,
+				'option_id' => 111,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 111,
+					'name' => 'seeking out old unhealthy people and places',
+					'category_id' => 6,
+				],
+			], [
+				'id' => 407,
+				'user_id' => 2,
+				'option_id' => 113,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 113,
+					'name' => 'people are angry with you',
+					'category_id' => 6,
+				],
+			], [
+				'id' => 408,
+				'user_id' => 2,
+				'option_id' => 122,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 122,
+					'name' => 'returning to the place you swore you would never go again',
+					'category_id' => 7,
+				],
+			], [
+				'id' => 409,
+				'user_id' => 2,
+				'option_id' => 123,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 123,
+					'name' => 'giving up',
+					'category_id' => 7,
+				],
+			], [
+				'id' => 410,
+				'user_id' => 2,
+				'option_id' => 124,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 124,
+					'name' => 'giving in',
+					'category_id' => 7,
+				],
+			], [
+				'id' => 411,
+				'user_id' => 2,
+				'option_id' => 125,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 125,
+					'name' => 'out of control',
+					'category_id' => 7,
+				],
+			], [
+				'id' => 412,
+				'user_id' => 2,
+				'option_id' => 126,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 126,
+					'name' => 'lost in your addiction',
+					'category_id' => 7,
+				],
+			], [
+				'id' => 413,
+				'user_id' => 2,
+				'option_id' => 127,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 127,
+					'name' => 'lying to yourself and others',
+					'category_id' => 7,
+				],
+			], [
+				'id' => 414,
+				'user_id' => 2,
+				'option_id' => 128,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 128,
+					'name' => 'feeling you just can\'t manage without your coping behavior, at least for now',
+					'category_id' => 7,
+				],
+			], [
+				'id' => 415,
+				'user_id' => 2,
+				'option_id' => 129,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 129,
+					'name' => 'shame',
+					'category_id' => 7,
+				],
+			], [
+				'id' => 416,
+				'user_id' => 2,
+				'option_id' => 130,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 130,
+					'name' => 'condemnation',
+					'category_id' => 7,
+				],
+			], [
+				'id' => 417,
+				'user_id' => 2,
+				'option_id' => 131,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 131,
+					'name' => 'guilt',
+					'category_id' => 7,
+				],
+			], [
+				'id' => 418,
+				'user_id' => 2,
+				'option_id' => 132,
+				'date' => '2016-06-21 04:08:36',
+				'option' => [
+					'id' => 132,
+					'name' => 'aloneness',
+					'category_id' => 7,
+				],
+			],
+		];
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        Yii::configure(Yii::$app, [
+            'components' => [
+                'user' => [
+                    'class' => 'yii\web\User',
+                    'identityClass' => 'tests\codeception\common\unit\FakeUser',
+                ],
+            ],
+        ]);
+
+        $identity = new \tests\codeception\common\unit\FakeUser();
+        $identity->timezone = "America/Los_Angeles";
+
+        // logs in the user 
+        Yii::$app->user->setIdentity($identity);
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testCalculateScore()
+    {
+      $this->specify('calculateScore should function correctly', function () {
+        expect('calculateScore should work with a single date item and simple behaviors', $this->assertEquals(UserOption::calculateScore($this->singleSimpleBehaviors), ['2016-06-16 21:12:43' => 29]));
+        expect('calculateScore should work with a single date item and complex behaviors', $this->assertEquals(UserOption::calculateScore($this->singleComplexBehaviors), ['2016-06-20 21:08:36' => 233]));
+      });
+    }
+}
