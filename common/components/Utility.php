d1b8292ccebf72c1b1d43e2c43c36bfc28fdd611
Corey Watts <corey@alexa.com>
2016-07-14 22:38:02 -0700

    Fixing little bug in Utility class and writing tests

--- common/components/Utility.php
+++ common/components/Utility.php
@@ -6,16 +6,17 @@ use yii;
 class Utility {
   public static $REVISION_FILE = "./REVISION";
 
-  public static function getGithubRevUrl() {
-    if($hash = self::getRevHash()) {
+  public static function getGithubRevUrl($file = false) {
+    if($hash = self::getRevHash($file)) {
       return "https://github.com/CorWatts/fasterscale/commit/$hash";
     }
     return false;
   }
 
-  public static function getRevHash() {
-    if(file_exists(self::$REVISION_FILE)) {
-      return substr(file_get_contents(self::$REVISION_FILE), 0, 7);
+  public static function getRevHash($file = false) {
+    $file = $file ?: self::$REVISION_FILE;
+    if(is_file($file) && is_readable($file)) {
+      return substr(file_get_contents($file), 0, 7);
     }
     return false;
   }
