aa7b9affa76820ad2ed964099d48cb6c1c16cb19
Corey Watts <corey@alexa.com>
2016-08-08 07:15:42 -0700

    Adding faq page

---
+++ site/views/site/faq.php
@@ -0,0 +1,19 @@
+<?php
+use yii\helpers\Html;
+use yii\jui\DatePicker;
+
+$this->title = 'The Faster Scale App | FAQ';
+$this->registerMetaTag([
+  'name' => 'description',
+  'content' => Html::encode("Have questions about the Faster Scale App? You might find your answers in this FAQ.")
+]);
+?>
+
+<div class="site-faq">
+  <h1>FAQ</h1>
+  <div>
+    <strong>Question?</strong>
+    <p class="indent">Answer.</p>
+  </div>
+
+</div>
