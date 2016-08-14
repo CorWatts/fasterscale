b907d62b632a09133159832797621aa32f5730ad
Corey Watts <corwatts@gmail.com>
2016-08-13 19:57:57 -0700

    WIP

---
+++ common/mail/checkinReport2.php
@@ -0,0 +1,189 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/app.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>My Email Subject</title>
+  </head>
+  <body>
+    <!-- <style> -->
+    <span class="preheader"></span>
+    <table class="body">
+      <tr>
+        <td class="center" align="center" valign="top">
+          <center data-parsed="">
+            
+              <table class="spacer float-center"><tbody><tr><td height="16px" style="font-size:16px;line-height:16px;">&#xA0;</td></tr></tbody></table> 
+            
+              <table align="center" class="container header float-center"><tbody><tr><td>
+                <table class="row"><tbody><tr>
+                      <th class="small-12 large-12 columns first last" valign="middle" style="background-color: #1FC28F;">
+      <table>
+        <tr>
+          <th>
+                    <h1 class="text-center" style="margin-bottom: 0;">Faster Scale App Report</h1>
+                  </th>
+<th class="expander"></th>
+        </tr>
+      </table>
+    </th>
+                </tr></tbody></table>
+                <table class="row"><tbody><tr>
+                      <th class="small-12 large-12 columns first last">
+      <table>
+        <tr>
+          <th>
+                    <p>Hello there! Erreth has set you as one of their report recipients.
+                    These reports are sent out when Erreth scores above 5 in their checkin.
+                    This means that they might be struggling emotionally, and you should
+                    contact them to see how they are. Their report results are below. You
+                    can reply to this email, and it will go directly to your friend.</p>
+                  </th>
+<th class="expander"></th>
+        </tr>
+      </table>
+    </th>
+                </tr></tbody></table>
+              </td></tr></tbody></table>
+            
+              <table class="spacer float-center"><tbody><tr><td height="16px" style="font-size:16px;line-height:16px;">&#xA0;</td></tr></tbody></table> 
+            
+              <table align="center" class="container questions float-center"><tbody><tr><td>
+                <table class="row"><tbody><tr>
+                      <th class="small-12 large-12 columns first last" valign="middle">
+      <table>
+        <tr>
+          <th>
+                    <h2 style="margin-bottom: 0;">Erreth's Responses:</h2>
+                  </th>
+<th class="expander"></th>
+        </tr>
+      </table>
+    </th>
+                </tr></tbody></table>
+                <table class="row"><tbody><tr>
+                      <th class="small-12 large-12 columns first last">
+      <table>
+        <tr>
+          <th>
+                <h4>changes in goals</h4>
+                <p><strong>How does it affect me? How do I act and feel?</strong> 
+                Look, just because I don't be givin' no man a foot massage don't make it
+                right for Marsellus to throw Antwone into a glass motherfuckin' house,
+                fuckin' up the way the nigger talks. Motherfucker do that shit to me, he
+                better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm
+                sayin'?</p>
+                <p><strong>How does it affect the important people in my life?</strong>
+                The path of the righteous man is beset on all sides by the iniquities
+                of the selfish and the tyranny of evil men. Blessed is he who, in the name
+                of charity and good will, shepherds the weak through the valley of darkness,
+                for he is truly his brother's keeper and the finder of lost children. And I
+                will strike down upon thee with great vengeance and furious anger those who
+                would attempt to poison and destroy My brothers. And you will know My name
+                is the Lord when I lay My vengeance upon thee.</p>
+                <p><strong>Why do I do this? What is the benefit for me?</strong>
+                ur bones don't break, mine do. That's clear. Your cells react to bacteria
+                and viruses differently than mine. You don't get sick, I do. That's also
+                clear. But for some reason, you and I react the exact same way to water. We
+                swallow it too fast, we choke. We get some in our lungs, we drown. However
+                unreal it may seem, we are connected, you and I. We're on the same curve,
+                just on opposite ends.</p>
+                <h4>being resentful</h4>
+                <p><strong>How does it affect me? How do I act and feel?</strong> 
+                Look, just because I don't be givin' no man a foot massage don't make it
+                right for Marsellus to throw Antwone into a glass motherfuckin' house,
+                fuckin' up the way the nigger talks. Motherfucker do that shit to me, he
+                better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm
+                sayin'?</p>
+                <p><strong>How does it affect the important people in my life?</strong>
+                The path of the righteous man is beset on all sides by the iniquities
+                of the selfish and the tyranny of evil men. Blessed is he who, in the name
+                of charity and good will, shepherds the weak through the valley of darkness,
+                for he is truly his brother's keeper and the finder of lost children. And I
+                will strike down upon thee with great vengeance and furious anger those who
+                would attempt to poison and destroy My brothers. And you will know My name
+                is the Lord when I lay My vengeance upon thee.</p>
+                <p><strong>Why do I do this? What is the benefit for me?</strong>
+                ur bones don't break, mine do. That's clear. Your cells react to bacteria
+                and viruses differently than mine. You don't get sick, I do. That's also
+                clear. But for some reason, you and I react the exact same way to water. We
+                swallow it too fast, we choke. We get some in our lungs, we drown. However
+                unreal it may seem, we are connected, you and I. We're on the same curve,
+                just on opposite ends.</p>
+                  </th>
+<th class="expander"></th>
+        </tr>
+      </table>
+    </th>
+                </tr></tbody></table>
+              </td></tr></tbody></table>
+            
+              <table class="spacer float-center"><tbody><tr><td height="16px" style="font-size:16px;line-height:16px;">&#xA0;</td></tr></tbody></table> 
+            
+              <table align="center" class="container behaviors float-center"><tbody><tr><td>
+                <table class="row"><tbody><tr>
+                      <th class="small-12 large-12 columns first last" valign="middle" style="padding: 10px;">
+      <table>
+        <tr>
+          <th>
+                    <h2 style="margin-bottom: 0;">Selected Emotions:</h2>
+                  </th>
+<th class="expander"></th>
+        </tr>
+      </table>
+    </th>
+                </tr></tbody></table>
+                <table class="row"><tbody><tr>
+                      <th class="small-12 large-12 columns first last">
+      <table>
+        <tr>
+          <th>
+                  <p><strong>Restoration</strong><br>
+                  resolving problems<br>
+                  reaching out to others</p>
+            
+                  <p><strong>Forgetting Priorities</strong><br>
+                  changes in goals<br>
+                  obsessed with relationships<br>
+                  preoccupation with material things, television, or entertainment</p>
+            
+                  <p><strong>Anxiety</strong><br>
+                  being resentful<br>
+                  trouble concentrating<br>
+                  seeking/creating drama<br>
+                  using over-the-counter medication for pain, sleep, and weight control<br>
+                  making goals and lists you can't complete</p>
+            
+                  <p><strong>Speeding Up</strong><br>
+                  can't relax<br>
+                  avoiding slowing down<br>
+                  feeling driven<br>
+                  overspending<br>
+                  can't identify own feelings/needs<br>
+                  repetitive, negative thoughts<br>
+                  too much caffeine<br>
+                  difficulty being alone or with people</p>
+                  </th>
+<th class="expander"></th>
+        </tr>
+      </table>
+    </th>
+                </tr></tbody></table>
+              </td></tr></tbody></table>
+            
+              <table class="spacer float-center"><tbody><tr><td height="16px" style="font-size:16px;line-height:16px;">&#xA0;</td></tr></tbody></table> 
+            
+          </center>
+        </td>
+      </tr>
+    </table>
+    <!-- prevent Gmail on iOS font size manipulation -->
+   <div style="display:none; white-space:nowrap; font:15px courier; line-height:0;"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </div>
+  </body>
+</html>
+
