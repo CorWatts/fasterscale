e3bf26d340dcaf4cd9f9a1491e7780a314cdd8ad
Corey Watts <corwatts@gmail.com>
2016-09-30 20:44:52 -0700

    Adding tests

--- tests/codeception/site/unit/models/QuestionFormTest.php
+++ tests/codeception/site/unit/models/QuestionFormTest.php
@@ -24,8 +24,23 @@ class QuestionFormTest extends TestCase
 
   public function testGetBhvrValidator()
   {
-    $this->specify('getBhvrValidator should function properly', function () {
-      $model = new QuestionForm();
+    $model = new QuestionForm();
+    $validator = $model->getBhvrValidator();
+
+    $this->specify('getBhvrValidator should function properly', function () use($model, $validator) {
+      expect('getBhvrValidator should return false when nothing is set on the form', $this->assertFalse($validator($model, "user_option_id1")));
+      expect('getBhvrValidator should return false when nothing is set on the form', $this->assertFalse($validator($model, "user_option_id7")));
+
+      $model->answer_1a = "processing emotions";
+      expect('getBhvrValidator should return true when there is one answer set for this option', $this->assertTrue($validator($model, "user_option_id1")));
+
+      $model->answer_1b = "also processing emotions";
+      expect('getBhvrValidator should return true when there are two answers set for this option', $this->assertTrue($validator($model, "user_option_id1")));
+
+      $model->answer_1c = "yep, processing emotions";
+      expect('getBhvrValidator should return true when there are three answers set for this option', $this->assertTrue($validator($model, "user_option_id1")));
+
+      expect('getBhvrValidator should return false when the answers that are set are NOT for this option', $this->assertFalse($validator($model, "user_option_id3")));
     });
   }
 }
