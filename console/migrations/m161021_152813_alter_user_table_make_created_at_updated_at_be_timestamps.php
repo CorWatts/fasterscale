44b07ae1e1826eea848975e27de36ce0a24dcbf1
Corey Watts <corwatts@gmail.com>
2016-10-23 20:29:41 -0700

    Starting this WIP

--- console/migrations/m161021_152813_alter_user_table_make_created_at_updated_at_be_timestamps.php
+++ console/migrations/m161021_152813_alter_user_table_make_created_at_updated_at_be_timestamps.php
@@ -6,6 +6,15 @@ class m161021_152813_alter_user_table_make_created_at_updated_at_be_timestamps e
 {
     public function safeUp()
     {
+      // rename these columns to be consistent
+      $this->renameColumn('{{%user_option_link}}', 'date', 'created_at');
+      $this->renameColumn('{{%question}}', 'date', 'created_at');
+
+      // add a timezone to these timestamp columns
+      $this->execute("SET LOCAL timezone='UTC';");
+      $this->alterColumn('{{%user_option_link}}', 'created_at', 'TIMESTAMP WITH TIMEZONE');
+      $this->alterColumn('{{%question}}', 'created_at', 'TIMESTAMP WITH TIMEZONE');
+
       $this->alterColumn('{{%user}}', 'created_at', "TIMESTAMP WITH TIME ZONE USING TIMESTAMP WITH TIME ZONE 'epoch' + created_at * interval '1 second';");
       $this->alterColumn('{{%user}}', 'updated_at', "TIMESTAMP WITH TIME ZONE USING TIMESTAMP WITH TIME ZONE 'epoch' + updated_at * interval '1 second';");
     }
@@ -14,7 +23,19 @@ class m161021_152813_alter_user_table_make_created_at_updated_at_be_timestamps e
     {
       echo "This doesn't work right now... :(\n";
       return false;
-      //$this->alterColumn("{{%user}}", "created_at", "INTEGER NOT NULL");
-      //$this->alterColumn("{{%user}}", "updated_at", "INTEGER NOT NULL");
+
+      // somehow reverse these
+      // select extract(epoch from created_at)::integer as created_at from "user";
+      //$this->alterColumn('{{%user}}', 'created_at', "TIMESTAMP WITH TIME ZONE USING TIMESTAMP WITH TIME ZONE 'epoch' + created_at * interval '1 second';");
+      //$this->alterColumn('{{%user}}', 'updated_at', "TIMESTAMP WITH TIME ZONE USING TIMESTAMP WITH TIME ZONE 'epoch' + updated_at * interval '1 second';");
+
+      // add a timezone to these timestamp columns
+      $this->execute("SET LOCAL timezone='UTC';");
+      $this->alterColumn('{{%user_option_link}}', 'created_at', 'TIMESTAMP WITHOUT TIMEZONE');
+      $this->alterColumn('{{%question}}', 'created_at', 'TIMESTAMP WITHOUT TIMEZONE');
+      
+      // rename these columns to be consistent
+      $this->renameColumn('{{%user_option_link}}', 'created_at', 'date');
+      $this->renameColumn('{{%question}}', 'created_at', 'date');
     }
 }
