fc7dbf7882238095087b2b13e0c37b030c6f40b9
Corey Watts <corwatts@gmail.com>
2016-11-05 10:08:02 -0700

    Fixes #40: Logged-in users are redirected when requesting a forbidden page
    Previously, a 403 error was thrown which wasn't very friendly. Now the
    user will get redirected to /checkin/index.

---
+++ common/components/AccessControl.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace common\components;
+
+use Yii;
+
+class AccessControl extends \yii\filters\AccessControl {
+  /**
+   * Denies the access of the user.
+   * The default implementation will redirect the user to the login page if he is a guest;
+   * if the user is already logged, they are redirected elsewhere
+   * @param User $user the current user
+   */
+  protected function denyAccess($user)
+  {
+    if ($user->getIsGuest()) {
+      $user->loginRequired();
+    } else {
+      // checkin/index is the route that users are redirected to
+      Yii::$app->getResponse()->redirect(['checkin/index'])->send();
+      return;
+    }
+  }
+}
