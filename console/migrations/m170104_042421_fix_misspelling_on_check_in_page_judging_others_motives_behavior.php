2da71ade8f43d0335dc56ef4beee1da0814c451a
Corey Watts <corwatts@gmail.com>
2017-01-03 20:30:53 -0800

    Fix misspelling on check in page: "Judging Others' Motives" behavior (#99)

---
+++ console/migrations/m170104_042421_fix_misspelling_on_check_in_page_judging_others_motives_behavior.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\db\Migration;
+
+class m170104_042421_fix_misspelling_on_check_in_page_judging_others_motives_behavior extends Migration
+{
+    public function safeUp()
+    {
+        $this->update("option", array("name"=>"judging others' motives"), array("name"=>"juding others' motives"));
+    }
+
+    public function safeDown()
+    {
+        $this->update("option", array("name"=>"juding others' motives"), array("name"=>"judging others' motives"));
+    }
+}
