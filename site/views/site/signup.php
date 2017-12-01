6be588ef341d2d4cbeccd6ee0226bcafb37dc378
Corey Watts <corey@haaswatts.com>
2017-12-01 08:47:59 -0800

    Improving the signup form with password hide/show functionality
    This is markedly better than it was, I think. Also, browsers aren't as
    likely to autocomplete it now.

--- site/views/site/signup.php
+++ site/views/site/signup.php
@@ -23,11 +23,11 @@ $this->registerMetaTag([
 			<?php $form = ActiveForm::begin([
 				'id' => 'form-signup',
 				'enableClientValidation' => true,
-    'enableAjaxValidation' => false,
-				'options' => ['validateOnSubmit' => true]
+        'enableAjaxValidation' => false,
+        'options' => [ 'validateOnSubmit' => true ]
 			]); ?>
         <?= $form->field($model, 'email', ['inputTemplate' => '<div class="input-group"><span class="input-group-addon">@</span>{input}</div>'])->input('email') ?>
-        <?= $form->field($model, 'password') ?>
+        <?= $form->field($model, 'password', ['inputTemplate' => '<div class="input-group">{input}<span class="input-group-btn"><button id="password-toggle" class="btn btn-default" type="button">Show</button></span></div>'])->passwordInput() ?>
         <?= $form->field($model, 'timezone')->dropDownList(array_combine($timezones, $timezones)); ?>
         <?= $form->field($model, 'captcha')->widget(Captcha::className(), [
           'template' => '<div class="row"><div class="col-md-5">{image}</div><div class="col-md-6 col-md-offset-1">{input}</div></div>',
@@ -35,9 +35,9 @@ $this->registerMetaTag([
         <?= $form->field($model, 'send_email')->checkbox() ?>
         <div id='email_threshold_fields' <?php if(!$model->send_email) { ?>style="display: none;"<?php } ?>>
           <?= $form->field($model, 'email_threshold', ['template' => '{label}<div class="row"><div class="col-md-3">{input}</div><div class="col-md-9"><p class="bg-info text-center" style="margin: 5px 0px;">We recommend starting with 30</p></div></div>{error}'])->textInput(['class'=>'form-control'])->input('number', ['min' => 0, 'max' => 100]) ?>
-          <?= $form->field($model, 'partner_email1')->input('email')->input('email') ?>
-          <?= $form->field($model, 'partner_email2')->input('email')->input('email') ?>
-          <?= $form->field($model, 'partner_email3')->input('email')->input('email') ?>
+          <?= $form->field($model, 'partner_email1')->input('email') ?>
+          <?= $form->field($model, 'partner_email2')->input('email') ?>
+          <?= $form->field($model, 'partner_email3')->input('email') ?>
         </div>
         <div class="form-group">
           <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
@@ -49,6 +49,20 @@ $this->registerMetaTag([
 
 <?php $this->registerJs(
   "$('#signupform-send_email').click(function() {
-    $('#email_threshold_fields').toggle();
+    if( $('#signupform-send_email').prop('checked') ) {
+      $('#email_threshold_fields').show();
+    } else {
+      $('#email_threshold_fields').hide();
+    }
+  });
+
+  $('#password-toggle').click(function () {
+    if( $('#signupform-password').attr('type') === 'password' ) {
+      $('#password-toggle').text('Hide');
+      $('#signupform-password').attr('type', 'text');
+    } else {
+      $('#password-toggle').text('Show');
+      $('#signupform-password').attr('type', 'password');
+    }
   });"
 );
